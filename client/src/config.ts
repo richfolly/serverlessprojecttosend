8677ec7a68dd957d3d24a5719a06fea07b6c0bf9
richfolly <follodeh65@gmail.com>
2022-07-19 22:33:55 +0000

    finalproject

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'lc8i6eupri'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-cr9isr4f.eu.auth0.com',           // Auth0 domain
+  clientId: 'so2GI2QmQaak1RzPnksGTsbsX1Nx9Oxv',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
